8da937cefa4c57553a01905ae7dda1b457e552f9
hoangnguyen9384 <ng_viethoang@yahoo.com>
2020-11-18 04:25:47 +0000

    complete react-elements

---
+++ react-elements/src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const h1 = React.createElement('h1', null, 'Hello React');
+// console.log(h1);
+ReactDOM.render(h1, document.getElementById('root'));

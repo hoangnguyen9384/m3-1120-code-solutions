d14107f734de5604143eb72a77a2aad6049b442a
hoangnguyen9384 <ng_viethoang@yahoo.com>
2020-12-07 08:05:55 +0000

    complete react-app-drawer-component

---
+++ react-app-drawer-component/src/app-drawer.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+
+export default class AppDrawer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isClicked: false
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(event) {
+    this.setState({ isClicked: true });
+  }
+
+  render() {
+    if (this.state.isClicked) {
+      return (
+        <body>
+          <div className="container-menu">
+            <ul>
+              <h2>Menu</h2>
+              <li>About</li>
+              <li>Get started</li>
+              <li>Sign In</li>
+            </ul>
+          </div>
+        </body>
+      );
+    } else {
+      return (
+        <div className= "container-menu-icon" onClick={this.handleClick}>
+          <div className="menu-icon"></div>
+          <div className="menu-icon"></div>
+          <div className="menu-icon"></div>
+        </div>
+
+      );
+    }
+  }
+}

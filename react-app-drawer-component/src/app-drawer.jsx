20e466154de9fd4efe5eabdc72e27746f3a501ea
hoangnguyen9384 <ng_viethoang@yahoo.com>
2020-12-09 07:13:09 +0000

    update react-app-drawer-component

--- react-app-drawer-component/src/app-drawer.jsx
+++ react-app-drawer-component/src/app-drawer.jsx
@@ -16,16 +16,14 @@ export default class AppDrawer extends React.Component {
   render() {
     if (this.state.isClicked) {
       return (
-        <body>
-          <div className="container-menu">
-            <ul>
-              <h2>Menu</h2>
-              <li>About</li>
-              <li>Get started</li>
-              <li>Sign In</li>
-            </ul>
-          </div>
-        </body>
+        <div className="container-menu">
+          <ul>
+            <h2>Menu</h2>
+            <li>About</li>
+            <li>Get started</li>
+            <li>Sign In</li>
+          </ul>
+        </div>
       );
     } else {
       return (

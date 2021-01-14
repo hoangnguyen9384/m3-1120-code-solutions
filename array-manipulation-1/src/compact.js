63d32a70d127761a16286e4e2bd6f842a999092b
hoangnguyen9384 <ng_viethoang@yahoo.com>
2021-01-14 16:20:57 +0000

    update change request

--- array-manipulation-1/src/compact.js
+++ array-manipulation-1/src/compact.js
@@ -2,8 +2,7 @@
 const compact = array => {
   const newArray = [];
   for (let i = 0; i <= array.length - 1; i++) {
-    if (array[i] !== false && array[i] !== null && array[i] !== 0 && array[i] &&
-      array[i] !== undefined && array[i] !== '') {
+    if (array[i]) {
       newArray.push(array[i]);
     }
   } return newArray;
